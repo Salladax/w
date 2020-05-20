9d0c2f936873459d24286aa923577d986e821901
Salladax <61566441+Salladax@users.noreply.github.com>
2020-05-19 23:32:06 -0300

    w
    w

---
+++ script.js
@@ -0,0 +1,23 @@
+details = document.querySelector("details")
+img = document.querySelector("details img")
+
+value = 0	// 0 = wa e 1 == wa
+notFirstTime = 0 //multiplicador para a primeira vez que rodar a imagem
+
+details.ontoggle = function () {
+	if (value == 0) {
+		setTimeout(function(){
+			img.src = "w.jpg"
+			value = 1
+
+			if (! notFirstTime) {
+				notFirstTime = 1
+			}
+		}, notFirstTime * 90) 
+
+	}
+	else {
+		img.src = "wa.jpg"
+		value = 0
+	}
+}
